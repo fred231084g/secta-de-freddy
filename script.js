a0e7c400516ee503299d1bfcae9f60169f256c71
fred231084g <freddy1984govoni@gmail.com>
2022-11-07 23:14:22 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-for(let i=0;i<=20;i+=0.5){
-  document.getElementById("freddy").innerHTML+=`<h3 style="filter: blur(${i}px);  color: #AB0000;text-align:center;">
-                debes de temerle a carlos 
-            </h3>`
-}
